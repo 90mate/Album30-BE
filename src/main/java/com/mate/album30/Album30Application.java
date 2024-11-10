336b7e6560aec7c019d7bb3a2dc06131160ac7e0
20210699 <20210699@duksung.ac.kr>
2024-11-10 22:01:38 +0900

    🎉 Init : init commit

---
+++ src/main/java/com/mate/album30/Album30Application.java
@@ -0,0 +1,13 @@
+package com.mate.album30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Album30Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Album30Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mate/album30/Album30ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mate.album30;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Album30ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
