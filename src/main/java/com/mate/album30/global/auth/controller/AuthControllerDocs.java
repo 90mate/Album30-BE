84c94b8b53df7c632202e38933c4f4240b7c1111
20210699 <20210699@duksung.ac.kr>
2024-12-05 05:33:57 +0900

    ♻️ Refactor: auth api 설명 수정

--- src/main/java/com/mate/album30/global/auth/controller/AuthControllerDocs.java
+++ src/main/java/com/mate/album30/global/auth/controller/AuthControllerDocs.java
@@ -4,8 +4,6 @@ import com.mate.album30.global.apiPayload.ApiResponse;
 import com.mate.album30.global.auth.dto.response.LoginDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.enums.ParameterIn;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Mono;
 
@@ -14,7 +12,7 @@ public interface AuthControllerDocs {
     @Operation(
             summary = "Kakao Login",
             parameters = {
-                    @Parameter(name = "Authorization", description = "kakao access token을 보냅니다. Bearer token 형식입니다.", required = true, in = ParameterIn.HEADER)
+                    @Parameter(name = "code", description = "kakao 인가코드를 보냅니다.", required = true)
             })
     public Mono<ApiResponse<LoginDto>> kakaoLogin(@RequestParam(value = "code") String token);
 
