007b8418ac482d4f8546556574b7bb21c8d786ed
Kang Chaewon <kang960953@gmail.com>
2024-12-09 18:08:02 +0900

    ✨ Feat: update chat
    Feat: update chat

--- src/main/java/com/mate/album30/domain/albumTalk/controller/AlbumTalkController.java
+++ src/main/java/com/mate/album30/domain/albumTalk/controller/AlbumTalkController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/chat")
+@RequestMapping("/chat")
 @RequiredArgsConstructor
 public class AlbumTalkController {
     final AlbumTalkService albumTalkService;
